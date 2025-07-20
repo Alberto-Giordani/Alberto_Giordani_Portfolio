22dae3fddd433c3dbc9ec1a9e2e2653485e351e0
Alberto Giordani <albertogiordani@ymail.com>
2025-07-20 19:58:17 +0200

    Modification pour rendre toutes les routes disponibles en ligne

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './router/App.jsx'
 
+if (sessionStorage.redirect) {
+  const redirect = sessionStorage.redirect
+  delete sessionStorage.redirect
+  window.history.replaceState(null, '', redirect)
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// Configuration du base path pour déployer sur GitHub Pages
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    fs: {
+      allow: ['.'],
+    },
+    historyApiFallback: true,
+  },
 })
